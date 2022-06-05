0bfc30b4412e2c8a6a45b5a943d9a5b19df955d2
Muhammad Arifin <muhammad.arifin1@ibm.com>
2022-06-05 20:54:57 +0700

    Added user struct & db connection

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"bwastartup/user"
+	"fmt"
+	"log"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func main() {
+	dsn := "user:@tcp(127.0.0.1:3306)/bwastartup?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	var users []user.User
+
+	db.Find(&users)
+
+	for _, user := range users {
+		fmt.Println(user.Name)
+		fmt.Println(user.Email)
+		fmt.Println("============")
+	}
+}
